e00cb6cbe17e5c9ac2eadfe38db994cc8258ac79
STNeto1 <gfreitasneto18@gmail.com>
2024-03-17 13:21:48 -0300

    feat: refactor sending messages

--- pkg/ws.go
+++ pkg/ws.go
@@ -17,7 +17,7 @@ type NotifyClientData struct {
 	Message []byte
 }
 
-var Clients = make(map[*websocket.Conn]*client)
+var clients = make(map[*websocket.Conn]*client)
 var Register = make(chan *websocket.Conn)
 var Unregister = make(chan *websocket.Conn)
 var Notify = make(chan NotifyClientData)
@@ -32,66 +32,48 @@ func (app *App) RunHub() {
 	for {
 		select {
 		case connection := <-Register:
-			Clients[connection] = &client{}
+			clients[connection] = &client{}
 
 		case message := <-Notify:
-			c, ok := Clients[message.Conn]
+			c, ok := clients[message.Conn]
 			if !ok {
 				log.Println("client not on map")
 				Unregister <- message.Conn
 				return
 			}
-			go func(client *client, msg *NotifyClientData) {
-				client.mu.Lock()
-				defer client.mu.Unlock()
 
-				if client.isClosing {
-					return
-				}
-
-				if err := WriteToConnection(msg.Conn, msg.Message); err != nil {
-					log.Println("write error:", err)
-
-					client.isClosing = true
-					Unregister <- msg.Conn
-				}
-			}(c, &message)
+			go WriteToConnection(message.Conn, c, message.Message)
 
 		case message := <-Broadcast:
 			// Send the message to all clients
-			for connection, c := range Clients {
-				go func(connection *websocket.Conn, c *client) { // send to each client in parallel so we don't block on a slow client
-					c.mu.Lock()
-					defer c.mu.Unlock()
-
-					if c.isClosing {
-						log.Println("is closing")
-						return
-					}
-
-					if err := WriteToConnection(connection, message); err != nil {
-						log.Println("write error:", err)
-
-						c.isClosing = true
-						Unregister <- connection
-					}
-				}(connection, c)
+			for connection, c := range clients {
+				go WriteToConnection(connection, c, message)
 			}
 
 		case connection := <-Unregister:
 			// Remove the client from the hub
-			delete(Clients, connection)
+			delete(clients, connection)
 		}
 	}
 }
 
-func WriteToConnection(conn *websocket.Conn, msg []byte) error {
+func WriteToConnection(conn *websocket.Conn, c *client, msg []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.isClosing {
+		log.Println("is closing")
+		return
+	}
+
 	if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
 		log.Println("write error:", err)
 
+		c.isClosing = true
+		Unregister <- conn
+
+		log.Println("write error:", err)
 		conn.WriteMessage(websocket.CloseMessage, []byte{})
 		conn.Close()
 	}
-
-	return nil
 }
